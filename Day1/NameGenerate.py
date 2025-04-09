b1c8fa32d7176419fdd90927bd7972aea69938fd
RanjitChavan1818 <ranjitchavan1050@gmail.com>
2025-04-09 20:49:38 +0530

    Add files via upload

---
+++ Day1/NameGenerate.py
@@ -0,0 +1,7 @@
+print("Welcome to the Brand Nmae Generator")
+print("What is your name of city where you grown")
+city=input()
+print("What is you pet name")
+pet=input()
+
+print("Your Brand nmae could be"+city+pet)
---
+++ Day1/first.py
@@ -0,0 +1,2 @@
+# This line of code take first input() then print overall result
+print("Hello "+input("What is your name?") +"!")
